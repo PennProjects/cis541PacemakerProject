0b18c5fb7a87bb8d1ba27d2a87c120a976780d8a
Micah Weitzman <mikachoow21@gmail.com>
2020-10-14 18:39:35 -0400

    Update spi_slave_test.cpp

--- spi_slave_test.cpp
+++ spi_slave_test.cpp
@@ -1,38 +1,28 @@
-/*
- * Copyright (c) 2006-2020 Arm Limited and affiliates.
- * SPDX-License-Identifier: Apache-2.0
- */
 #include "mbed.h"
-#include "TextLCD.h"
-
-SPI spi(p11, p12, p13); // mosi, miso, sclk
-DigitalOut cs(p5); // set PIN5 to ChipSelect Pin 
-TextLCD lcd(p15, p16, p17, p18, p19, p20, TextLCD::LCD16x2);  // SETUP LCD 
+ 
+SPISlave device(p11, p12, p13, p14); // mosi, miso, sclk, ChipSelect
 Serial pc(USBTX, USBRX); // tx, rx
-
+ 
 int main() {
  
-    int valueToSendToSlave = 20; // Starting value only, this increments
-    spi.format(8,3);        // Setup:  bit data, high steady state clock, 2nd edge capture
-    spi.frequency(1000000); //1MHz
+    int counter = 1;
+ 
+    device.format(8,3);        // Setup:  bit data, high steady state clock, 2nd edge capture
+    device.frequency();  // 1MHz clockrate 
+ 
+    int reply = 99;
+    device.reply(reply);              // Prime SPI with first reply
+    device.reply(reply);              // Prime SPI with first reply, again
  
     pc.printf("======================================================\r\n");
-    pc.printf("Press any key to start...\r\n");
-    pc.getc(); // wait for keyboard
+    pc.printf("Startup Next reply will be %d\r\n", reply);
  
-    int counter = 1;
     while (1) {
-
-        cs = 0; // Select device
-        int dataFromSlave =  spi.write(valueToSendToSlave); //send value
-        cs = 1; // Deselect device
-        lcd.cls(); // clear LCD screen 
-        lcd.printf("Value to send = %d ",valueToSendToSlave);
-        lcd.printf("returns %d\r", dataFromSlave);
-        
-        valueToSendToSlave++;
-        
-        wait_us(2000000); // Wait for 2 seconds for readability only
-        
+        if (device.receive()) {
+            int valueFromMaster = device.read(); // read value from master 
+            pc.printf("%d Something rxvd, and should have replied with %d\n\r", counter++, reply);
+            device.reply(++reply);              // Prime SPI with next reply
+            pc.printf("    Received value from Master (%d) Next reply will be %d \r\n", valueFromMaster, reply);
+        }
     }
 }
